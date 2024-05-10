f77c3845c2bbe72fafd74eb0a24ea7033cd03a38
Erbynn John <john.erbynn@gmail.com>
2024-05-09 19:15:58 -0500

    Initial commit

---
+++ src/main/kotlin/com/example/ordrsmartapi/HelloController.kt
@@ -0,0 +1,11 @@
+package com.example.ordrsmartapi
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("/")
+    fun hello(): String = "Hello, Spring Boot with Kotlin!"
+}
