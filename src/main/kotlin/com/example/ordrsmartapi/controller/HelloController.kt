df7ca22a932281ae3c7546d52a7d7864173145ac
Erbynn John <john.erbynn@gmail.com>
2024-05-12 02:41:22 -0500

    fix failing unit test

--- src/main/kotlin/com/example/ordrsmartapi/controller/HelloController.kt
+++ src/main/kotlin/com/example/ordrsmartapi/controller/HelloController.kt
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
-    @GetMapping("/")
+    @GetMapping("/hello")
     fun hello(): String = "Hello, Spring Boot with Kotlin..."
 }
